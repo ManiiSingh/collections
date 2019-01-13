9eeea2aab4b9cc6e3d183aefc5e4b5062fffa263
Manjeet <manii.singh4@gmail.com>
2019-01-13 11:01:52 +0530

    Pushing the Changes thrugh git

--- Collection/src/listCollection/ArrayListConcept.java
+++ Collection/src/listCollection/ArrayListConcept.java
@@ -6,7 +6,7 @@ public class ArrayListConcept {
 		// TODO Auto-generated method stub
 		int[] a = new int[3];//Static array
 		//<---dynamic array --->		
-
+//<--Adding a comment and pushing the changes thorugh git -->
 	}
 
 }
