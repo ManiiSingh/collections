1a0ccca251cd1e7ebe1de8bc85097078bba82e08
Manjeet <manii.singh4@gmail.com>
2019-01-13 10:35:30 +0530

    First commit and push of my collection Java Project

---
+++ Collection/src/listCollection/ArrayListConcept.java
@@ -0,0 +1,12 @@
+package listCollection;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[3];//Static array
+		//<---dynamic array --->		
+
+	}
+
+}
