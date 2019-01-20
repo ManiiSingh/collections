fc4c3b3fc7d76aeafbfc024a6bf54f6074e81c53
Manjeet <manii.singh4@gmail.com>
2019-01-20 10:42:45 +0530

    Implementation of char count java class

--- Collection/src/listCollection/CharNumber.java
+++ Collection/src/listCollection/CharNumber.java
@@ -1,10 +1,35 @@
 package listCollection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CharNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str = "ManjeetSingh";
+		getcharcount(str);
 
+	}
+
+	private static void getcharcount(String str) {
+		// TODO Auto-generated method stub
+		HashMap<Character,Integer> charcount = new HashMap<>();
+		char[] str1 = str.toCharArray();
+		for(char c: str1) {
+			if(charcount.containsKey(c)) {
+				charcount.put(c, charcount.get(c)+1);
+			}
+			else {
+				charcount.put(c, 1);
+			}
+			for(Map.Entry entry : charcount.entrySet()) {
+				System.out.println(entry.getKey() + ":" + entry.getValue());
+			}
+		}
+		
 	}
 
 }
